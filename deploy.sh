3ca4ba5df129e981ec5d9ba16e5d24ad93b98c0a
Rafael Peria de Sene <rpsene@br.ibm.com>
2021-03-19 21:52:30 -0300

    Add support to use openshift-install-powervs
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 : '
-    Copyright (C) 2020,2021 IBM Corporation
+    Copyright (C) 2020, 2021 IBM Corporation
     Rafael Sene <rpsene@br.ibm.com> - Initial implementation.
 '
 
@@ -34,7 +34,7 @@ function check_variables() {
 		VAR=$(echo "$line" | awk '{split($0,var,"="); print var[1]}')
 		VALUE=$(echo "$line" | awk '{split($0,var,"="); print var[2]}')
 
-		if [ -z $VALUE ]; then
+		if [ -z "$VALUE" ]; then
 	  		echo
 	   		echo "ERROR: $VAR is not set."
 	   		echo "      check the $INPUT file and try again."
@@ -47,7 +47,8 @@ function check_variables() {
 function check_connectivity() {
     
     curl --output /dev/null --silent --head --fail http://github.com
-    if [ ! $? -eq 0 ]; then
+	CURL_EXIT=$?
+    if [ ! $CURL_EXIT -eq 0 ]; then
         echo
         echo "ERROR: please, check your internet connection."
         exit 1
@@ -58,21 +59,20 @@ function configure() {
 
 	OCP_VERSION=$1
 
-	if [ -s ./ocp-secrets ]; then
+	if [ -s "$(pwd)"/files/ocp-secrets ]; then
 
-		mkdir -p ./powervs-clusters; cd ./powervs-clusters
-		
-		DIR=$(echo "ocp-"$OCP_VERSION"-"$SUFIX | tr -d .)
+		mkdir -p ./powervs-clusters; cd ./powervs-clusters || exit
 
-		git clone --single-branch --branch release-"$OCP_VERSION" \
-		https://github.com/ocp-power-automation/ocp4-upi-powervs.git $DIR
-		
-		ssh-keygen -t rsa -b 4096 -N '' -f ./$DIR/data/id_rsa
+		DIR=$(echo "ocp-""$OCP_VERSION""-""$SUFIX" | tr -d .)
+		mkdir -p ./"$DIR"
+		mkdir -p ./"$DIR"/data
+		mkdir -p ./"$DIR"/scripts
+		mkdir -p ./"$DIR"/cluster-size/
 
-		cat ../ocp-secrets >> ./$DIR/data/pull-secret.txt
-
-		cp -rp ../scripts/run-terraform.sh ./$DIR
-		cp -rp ../scripts/cluster-access-information.sh ./$DIR
+		cat ../files/ocp-secrets >> ./"$DIR"/data/pull-secret.txt
+		cp -rp ../scripts/run-terraform.sh ./"$DIR"/scripts
+		cp -rp ../scripts/cluster-access-information.sh ./"$DIR"/scripts
+		cp -rp ../files/cluster-size/* ./"$DIR"/cluster-size/
 	else
 		echo
 		echo "ERROR: ensure you added the OpenShift Secrets at ./ocp-secrets"	
@@ -83,56 +83,62 @@ function configure() {
 }
 
 function create_container (){
-
+	
+	local CONTAINER_NAME
+	local PREFIX
+	local DIR
 	local OCP_VERSION=$1
-	local CONTAINER_NAME=$(echo "ocp-"$OCP_VERSION"-"$SUFIX | tr -d .)
-	local PREFIX=$(echo "ocp-"$OCP_VERSION | tr -d .)
-	local DIR=$(echo "ocp-"$OCP_VERSION"-"$SUFIX | tr -d .)
 
-	cp -rp ../variables ./tmp-variables
+	CONTAINER_NAME=$(echo "ocp-$OCP_VERSION-$SUFIX" | tr -d .)
+	PREFIX=$(echo "ocp-$OCP_VERSION" | tr -d .)
+	DIR=$(echo "ocp-$OCP_VERSION-$SUFIX" | tr -d .)
+
+	cp -rp ../files/variables ./tmp-variables
 
 	sed -i -e "s/sufix/$SUFIX/g" ./tmp-variables
 	sed -i -e "s/prefix/$PREFIX/g" ./tmp-variables
 
-	mv ./tmp-variables ./$DIR/$CONTAINER_NAME-variables
+	mv ./tmp-variables ./"$DIR"/data/"$CONTAINER_NAME"-variables
 
 	# starts the base container with the basic set of env vars
-	$CONTAINER_RUNTIME run -dt --name $CONTAINER_NAME \
-	-v "$(pwd)"/$DIR:/ocp4-upi-powervs --env-file ./$DIR/$CONTAINER_NAME-variables \
-	quay.io/powercloud/powervs-container-host:ocp-$OCP_VERSION /bin/bash
+	$CONTAINER_RUNTIME run -dt --name "$CONTAINER_NAME" \
+	-v "$(pwd)"/"$DIR":/ocp4-upi-powervs -e RELEASE_VER="$OCP_VERSION" --env-file ./"$DIR"/data/"$CONTAINER_NAME"-variables \
+	quay.io/powercloud/powervs-container-host:ocp-"$OCP_VERSION" /bin/bash
 
 	echo "*********************************************************************************"
 	echo "NOTE: the installation is running from within the container named $CONTAINER_NAME"
 	echo "*********************************************************************************"
 
 	# execute the TF deployment from within the container
-	$CONTAINER_RUNTIME exec --tty -w /ocp4-upi-powervs $CONTAINER_NAME bash -c "./run-terraform.sh"
+	$CONTAINER_RUNTIME exec --tty -w /ocp4-upi-powervs "$CONTAINER_NAME" bash -c "./scripts/run-terraform.sh"
 }
 
 function run (){
 
 	OCP_VERSIONS=("4.5" "4.6" "4.7")
 
-	if [ -z $1 ]; then
+	if [ -z "$1" ]; then
 		echo
-		echo "ERROR: please, select one of the supported versions: ${OCP_VERSIONS[@]}."
+		echo "ERROR: Please, select one of the supported versions: ${OCP_VERSIONS[*]}."
 		echo "       e.g: ./deploy 4.7"
 		echo
 		exit 1
-	elif [[ ! " ${OCP_VERSIONS[@]} " =~ " ${1} " ]]; then
+	elif [[ ! " ${OCP_VERSIONS[*]} " =~ ${1} ]]; then
 		echo
-		echo "ERROR: this version of OpenShift ($1) is not supported."
-		echo "       pick one of the following: ${OCP_VERSIONS[@]}."
+		echo "ERROR: This version of OpenShift ($1) is not supported."
+		echo "       Supported versions are: ${OCP_VERSIONS[*]}."
 		echo
 		exit 1
 	else
-		export TODAY=$(date "+%Y%m%d")
-		export SUFIX=$(openssl rand -hex 3)
+		TODAY=$(date "+%Y%m%d")
+		SUFIX=$(openssl rand -hex 3)
+		export TODAY
+		export SUFIX
 		check_dependencies
-		check_variables ./variables
+		check_variables "$(pwd)"/files/variables
 		check_connectivity
-		configure $1
-		create_container $1
+		configure "$1"
+		create_container "$1"
 	fi
 }
 
--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 : '
-    Copyright (C) 2020,2021 IBM Corporation
+    Copyright (C) 2020, 2021 IBM Corporation
     Rafael Sene <rpsene@br.ibm.com> - Initial implementation. 
 '
 
@@ -16,8 +16,9 @@ function ctrl_c() {
 
 function check_connectivity() {
     
-    curl --output /dev/null --silent --head --fail http://cloud.ibm.com
-    if [ ! $? -eq 0 ]; then
+    curl --output /dev/null --silent --head --fail http://github.com
+	CURL_EXIT=$?
+    if [ ! $CURL_EXIT -eq 0 ]; then
         echo
         echo "ERROR: please, check your internet connection."
         exit 1
@@ -26,88 +27,87 @@ function check_connectivity() {
 
 function terraform_create (){
 
-	terraform init
-	# check if Terraform was initiaded successfully.
-	if [ $? -eq 0 ]; then
-		echo
-		echo "SUCCESS: Terraform was initiated successfully."
-		echo
+	curl -sL https://raw.githubusercontent.com/ocp-power-automation/openshift-install-power/master/openshift-install-powervs -o ./openshift-install-powervs
 
-		# run Terraform apply
-		time terraform apply -auto-approve -var-file var.tfvars \
-		-var-file compute-vars/"$CLUSTER_FLAVOR".tfvars \
-		-var ibmcloud_api_key="$IBMCLOUD_API_KEY" \
-		-var ibmcloud_region="$IBMCLOUD_REGION" \
-		-var ibmcloud_zone="$IBMCLOUD_ZONE" \
-		-var service_instance_id="$POWERVS_INSTANCE_ID" \
-		-var rhel_image_name="$BASTION_IMAGE_NAME" \
-		-var rhcos_image_name="$RHCOS_IMAGE_NAME" \
-		-var processor_type="$PROCESSOR_TYPE" \
-		-var system_type="$SYSTEM_TYPE" \
-		-var network_name="$PRIVATE_NETWORK_NAME" \
-		-var rhel_subscription_username="$RHEL_SUBS_USERNAME" \
-		-var rhel_subscription_password="$RHEL_SUBS_PASSWORD" \
-		-var cluster_id="$CLUSTER_ID" \
-		-var cluster_id_prefix="$CLUSTET_ID_PREFIX" \
-		-var bastion_health_status="$BASTION_HEALTH" \
-		-var cluster_domain="$CLUSTER_DOMAIN" | tee create.log
+	if [ -f "openshift-install-powervs" ]; then
 
+		chmod +x ./openshift-install-powervs
+
+		VARS=(
+		"ibmcloud_api_key = \"$IBMCLOUD_API_KEY\""
+		"ibmcloud_region = \"$IBMCLOUD_REGION\""
+		"ibmcloud_zone = \"$IBMCLOUD_ZONE\""
+		"service_instance_id = \"$POWERVS_INSTANCE_ID\""
+		"rhel_image_name = \"$BASTION_IMAGE_NAME\""
+		"rhcos_image_name = \"$RHCOS_IMAGE_NAME\""
+		"processor_type = \"$PROCESSOR_TYPE\""
+		"system_type = \"$SYSTEM_TYPE\""
+		"network_name = \"$PRIVATE_NETWORK_NAME\""
+		"cluster_id = \"$CLUSTER_ID\""
+		"cluster_id_prefix = \"$CLUSTET_ID_PREFIX\""
+		"bastion_health_status = \"$BASTION_HEALTH\""
+		"cluster_domain = \"$CLUSTER_DOMAIN\""
+		"storage_type = \"$STORAGE_TYPE\""
+		"volume_size = \"$STORAGE_VOLUME_SIZE\""
+		"volume_type = \"$STORAGE_VOLUME_TYPE\""
+		"volume_shareable = \"$STORAGE_VOLUME_SHAREABLE\""
+		)
+
+		# if the user is going to use RHEL as bastion
+		if [ "$RHEL_SUBS_USERNAME" ] && [ "$RHEL_SUBS_PASSWORD" ]; then
+			VARS=( "${VARS[@]}" "rhel_subscription_username = \"$RHEL_SUBS_USERNAME\"" )
+			VARS=( "${VARS[@]}" "rhel_subscription_username = \"$RHEL_SUBS_PASSWORD\"" )
+		fi
+
+		# add the variables for the size of the cluster
+		if [ "$CLUSTER_FLAVOR" ]; then
+			SIZE=$(cat ./cluster-size/"$CLUSTER_FLAVOR")
+			VARS=( "${VARS[@]}" "$SIZE" )
+		fi
+
+		# create the var.tfvars that will be used as input for the automation
+		printf '%s\n' "${VARS[@]}" >> var.tfvars
+
+		# move the pull secret to where it is expected
+		mv ./data/pull-secret.txt ./
+
+		./openshift-install-powervs create -verbose -var-file ./var.tfvars | tee create.log
+
+		OCP_INSTALL_EXIT=$?
 		# check if terraform apply was successfuly executed.
-		if [ $? -eq 0 ]; then
+		if [ "$OCP_INSTALL_EXIT" -eq 0 ]; then
 			echo
 			echo "SUCCESS: Terraform apply was executed successfully."
 			echo
-
-			local BASTION_IP=$(terraform output --json | jq -r '.bastion_public_ip.value')
-			local BASTION_SSH=$(terraform output --json | jq -r '.bastion_ssh_command.value')
+			cd ./automation || exit 1
+			local BASTION_IP
+			local BASTION_SSH
+			BASTION_IP=$(terraform output --json | jq -r '.bastion_public_ip.value')
+			BASTION_SSH=$(terraform output --json | jq -r '.bastion_ssh_command.value')
 
 			# check whether or not we can ssh into the bastion
 			$BASTION_SSH -oStrictHostKeyChecking=no 'exit'
-			if [ $? -eq 0 ]; then
+			SSH_EXIT=$?
+			if [ "$SSH_EXIT" -eq 0 ]; then
 				echo
 				echo "SUCCESS: we are able to ssh into the bastion."
 				echo
+				local BASTION_HOSTNAME
+				local CLUSTER_ID
+				local KUBEADMIN_PWD
+				local WEBCONSOLE_URL
+				local OCP_SERVER_URL
 
-				local BASTION_HOSTNAME=$($BASTION_SSH -oStrictHostKeyChecking=no 'hostname')
-				local CLUSTER_ID=$(terraform output --json | jq -r '.cluster_id.value')
-				local KUBEADMIN_PWD=$($BASTION_SSH -oStrictHostKeyChecking=no 'cat ~/openstack-upi/auth/kubeadmin-password; echo')
-				local WEBCONSOLE_URL=$(terraform output --json | jq -r '.web_console_url.value')
-				local OCP_SERVER_URL=$(terraform output --json | jq -r '.oc_server_url.value')
+				BASTION_HOSTNAME=$($BASTION_SSH -oStrictHostKeyChecking=no 'hostname')
+				CLUSTER_ID=$(terraform output --json | jq -r '.cluster_id.value')
+				KUBEADMIN_PWD=$($BASTION_SSH -oStrictHostKeyChecking=no 'cat ~/openstack-upi/auth/kubeadmin-password; echo')
+				WEBCONSOLE_URL=$(terraform output --json | jq -r '.web_console_url.value')
+				OCP_SERVER_URL=$(terraform output --json | jq -r '.oc_server_url.value')
 				# copies the authentication files from the bastion
 				local AUTH_FILES="auth_files.tgz"
 				$BASTION_SSH -oStrictHostKeyChecking=no 'cd ~/openstack-upi && tar -cf - * | gzip -9' > $AUTH_FILES
-		
+
 				mkdir -p ./"$CLUSTER_ID"-access-details
-
-				echo "
-					CLUSTER ACCESS INFORMATION
-					Cluster ID: $CLUSTER_ID
-					Cluster Size: $CLUSTER_FLAVOR
-					Bastion IP: $BASTION_IP ($BASTION_HOSTNAME)
-					Bastion SSH: $BASTION_SSH
-					OpenShift Access (user/pwd): kubeadmin/$KUBEADMIN_PWD
-					Web Console: $WEBCONSOLE_URL
-					OpenShift Server URL: $OCP_SERVER_URL
-					Kubeconfig: $AUTH_FILES
-					****************************
-					IBM Cloud Region=$IBMCLOUD_REGION
-					IBM Cloud Zone=$IBMCLOUD_ZONE
-					PowerVS ID=$POWERVS_INSTANCE_ID
-					Bastion Image=$BASTION_IMAGE_NAME
-					RedHat CoreOS Image=$RHCOS_IMAGE_NAME
-					Processor Type=$PROCESSOR_TYPE
-					System Model=$SYSTEM_TYPE
-					Private Network Name=$PRIVATE_NETWORK_NAME
-				" >> ./"$CLUSTER_ID"-access-details/access-details
-
-				mv ./auth_files.tgz ./"$CLUSTER_ID"-access-details
-				cp -rp ./create.log ./"$CLUSTER_ID"-access-details
-				cp -rp ./$CLUSTER_ID-variables ./"$CLUSTER_ID"-access-details
-				
-				mkdir -p ./"$CLUSTER_ID"-access-details/ssh-key
-				cp -rp ./data/id_rsa* ./"$CLUSTER_ID"-access-details/ssh-key
-
-				tar -czvf "$CLUSTER_ID"-access-details.tar ./"$CLUSTER_ID"-access-details
 
 				ACCESS_INFO=(
 				"**************************************************************"
@@ -119,8 +119,8 @@ function terraform_create (){
 				"OpenShift Access (user/pwd): kubeadmin/$KUBEADMIN_PWD"
 				"Web Console: $WEBCONSOLE_URL"
 				"OpenShift Server URL: $OCP_SERVER_URL"
-				"Kubeconfig: $AUTH_FILES"
-				"***********************"
+				"Kubeconfig: /$CLUSTER_ID-access-details/kubeconfig"
+				"**************************************************************"
 				"IBM Cloud Region=$IBMCLOUD_REGION"
 				"IBM Cloud Zone=$IBMCLOUD_ZONE"
 				"PowerVS ID=$POWERVS_INSTANCE_ID"
@@ -131,21 +131,38 @@ function terraform_create (){
 				"Private Network Name=$PRIVATE_NETWORK_NAME"
 				"**************************************************************"
 				)
+
+				printf '%s\n' "${ACCESS_INFO[@]}" >> ./"$CLUSTER_ID"-access-details/access-details
+
+				mkdir -p /tmp/"$CLUSTER_ID"
+
+				tar -xf ./auth_files.tgz --directory /tmp/"$CLUSTER_ID"
+
+				cp -rp /tmp/"$CLUSTER_ID"/auth/* ./"$CLUSTER_ID"-access-details
+				cp -rp ../create.log ./"$CLUSTER_ID"-access-details
+				cp -rp ../var.tfvars ./"$CLUSTER_ID"-access-details
+				cp -rp "../$CLUSTER_ID-variables" ./"$CLUSTER_ID"-access-details
+				
+				mkdir -p ./"$CLUSTER_ID"-access-details/ssh-key
+				cp -rp ./data/id_rsa* ./"$CLUSTER_ID"-access-details/ssh-key
+
+				tar -czvf "$CLUSTER_ID"-access-details.tar ./"$CLUSTER_ID"-access-details
+
 				printf '%s\n' "${ACCESS_INFO[@]}"
 			else
 				echo
-				echo "ERROR: we are not able to ssh into the bastion."
-				echo "		 your cluster deployment failed!"
+				echo "ERROR: We are not able to ssh into the bastion."
+				echo "		 Your cluster deployment failed :("
 				exit 1
 			fi
 		else
 			echo
-			echo "ERROR: Terraform apply failed."
+			echo "ERROR: Terraform apply failed, aborting."
 			exit 1
 		fi
 	else
 		echo
-		echo "ERROR: Terraform init failed."
+		echo "ERROR: Could not locate $INSTALL, aborting."
 		exit 1
 	fi
 }
