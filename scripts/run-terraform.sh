b0f86dd4b30ed134b8628f45c59c8f69da3ce250
Rafael Sene <rpsene@br.ibm.com>
2021-05-03 10:46:08 -0300

    Update run-terraform.sh

--- scripts/run-terraform.sh
+++ scripts/run-terraform.sh
@@ -141,7 +141,7 @@ function terraform_create (){
 				cp -rp /tmp/"$CLUSTER_ID"/auth/* ./"$CLUSTER_ID"-access-details
 				cp -rp ../create.log ./"$CLUSTER_ID"-access-details
 				cp -rp ../var.tfvars ./"$CLUSTER_ID"-access-details
-				cp -rp "../$CLUSTER_ID-variables" ./"$CLUSTER_ID"-access-details
+				#cp -rp "../$CLUSTER_ID-variables" ./"$CLUSTER_ID"-access-details
 				
 				mkdir -p ./"$CLUSTER_ID"-access-details/ssh-key
 				cp -rp ./data/id_rsa* ./"$CLUSTER_ID"-access-details/ssh-key
